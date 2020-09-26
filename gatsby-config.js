3356067694815fadeee281a745c306b27898b40e
vicotte <vicdugue@gmail.com>
2020-09-27 00:52:03 +0200

    essais 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,18 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
+const path = require(`path`)
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,28 @@
 import React from "react"
 import Header from "../components/header"
 
-import photo from "../../media/real.jpg"
+import photo from "../images/real.jpg"
+import { graphql } from "gatsby"
 
-export default function About() {
+import Img from "gatsby-image"
+
+export default function About({ data }) {
   return (
     <div style={{ color: `teal` }}>
       <Header headerText="hello Victor" />
       <p>Such wow. Very React.</p>
-      <img src={photo} />
+      <Img fluid={data.zigoto.childImageSharp.fluid} />
     </div>
   )
 }
+export const query = graphql`
+  {
+    zigoto: file(relativePath: { eq: "real.jpg" }) {
+      childImageSharp {
+        fluid(maxHeight: 350) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
